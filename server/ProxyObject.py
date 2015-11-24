1424d01ecfde53fabce34485eb3f0767c199cf21
Kevin Rojczyk <kevin.roj@p8h.de>
2015-11-24 16:31:19 +0100

    improved test_IPC and corrected ProxyObject

--- server/ProxyObject.py
+++ server/ProxyObject.py
@@ -1,10 +1,11 @@
-class ProxyObject(object):
-    __id = 0
+from abc import ABCMeta
+
+
+class ProxyObject(metaclass=ABCMeta):
 
     def __init__(self):
         self.__id = id(self)
 
-    @property
     def get_id(self):
         """Get the ID from ProxyObject for reconstruction on the server"""
         return self.__id
