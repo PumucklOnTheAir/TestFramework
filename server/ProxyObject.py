7d973c756d49d404de2dfdbb1b568fa4d29e4b7c
Kevin Rojczyk <kevin.roj@p8h.de>
2015-11-25 11:36:00 +0100

    enhanced documentation of IPC model

--- server/ProxyObject.py
+++ server/ProxyObject.py
@@ -2,7 +2,13 @@ from abc import ABCMeta
 
 
 class ProxyObject(metaclass=ABCMeta):
-
+    """" This class needed to share objects over IPC.
+        ProxyObject saves on the server his id which marks the memory point on the server.
+        The remote client get this object by copy so the reference is lost for the server.
+        If the server get this object back, he can reconstruct reference to the origin object.
+        But watch out in the reconstruction process!
+        You have to proof that the reference is valid and the origin object still exist on the server.
+    """""
     def __init__(self):
         self.__id = id(self)
 
