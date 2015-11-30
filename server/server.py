d4de5cee83ac57e1bb4816bbd143d60afe78c667
Kevin Rojczyk <kevin.roj@p8h.de>
2015-11-30 20:03:48 +0100

    solved problems with IPC and Server

--- server/server.py
+++ server/server.py
@@ -13,38 +13,37 @@ class Server(ServerProxy):
     _runningTests = []
     _routers = []
     _reports = []
-    #config = Configuration("")
 
-    def start(self, debug_mode: bool = False, config_path: str = CONFIG_PATH):
+    @classmethod
+    def start(cls, debug_mode: bool = False, config_path: str = CONFIG_PATH):
         assert isinstance(debug_mode, bool)
+        cls.DEBUG = debug_mode
 
-        self.DEBUG = debug_mode
+        assert isinstance(config_path, str)
+        cls.CONFIG_PATH = config_path
 
-        assert isinstance(config_path, str) # TODO funktioniert das?
-        self.CONFIG_PATH = config_path
+        cls._ipc_server.start_ipc_server(cls)
 
-        #self._ipc_server.start_ipc_server(self)
-
-        self.__load_configuration()
+        cls.__load_configuration()
 
         # TODO load more Router information
         # -> (Router[])
 
-    def __load_configuration(self):
+    @classmethod
+    def __load_configuration(cls):
         # (re)load the configuration only then no tests are running
-        assert len(self._runningTests) == 0
-        # TODO how to load the configuration, #16
-        self._routers = ConfigManager.get_vlan_list()
-        assert len(self._routers) != 0
-        assert len(self._reports) == 0
+        assert len(cls._runningTests) == 0
+        cls._routers = ConfigManager.get_vlan_list()
+        assert len(cls._routers) != 0
+        assert len(cls._reports) == 0
 
+    @classmethod
+    def stop(cls):
+        cls._ipc_server.shutdown()
         pass
 
-    def stop(self):
-        # TODO self._ipc_server.shutdown()
-        pass
-
-    def start_test(self, router_name, test_name) -> bool:
+    @classmethod
+    def start_test(cls, router_name, test_name) -> bool:
         """Start an specific test on an router
         :param router_name: The name of the router on which the test will run
         :param test_name: The name of the test to execute
@@ -54,36 +53,40 @@ class Server(ServerProxy):
         #_runningTests.add(Thread.start(test, router))
         pass
 
-    def get_routers(self) -> []:
+    @classmethod
+    def get_routers(cls) -> []:
         """
         :return: List of known routers. List is a copy of the original list.
         """
 
         # check if list is still valid
-        for router in self._routers:
+        for router in cls._routers:
             assert isinstance(router, Router)
 
-        return self._routers.copy()
+        return cls._routers.copy()
 
-    def get_running_tests(self) -> []:
+    @classmethod
+    def get_running_tests(cls) -> []:
         """
         :return: List of running test on the test server. List is a copy of the original list.
         """
-        return self._runningTests.copy()
+        return cls._runningTests.copy()
 
-    def get_reports(self) -> []:
+    @classmethod
+    def get_reports(cls) -> []:
         """
         :return: List of reports
         """
-        return self._reports
-
-    def get_tests(self) -> []:
+        return cls._reports
+    @classmethod
+    def get_tests(cls) -> []:
         """
         :return: List of available tests on the server
         """
         pass # TODO get available test from config
 
-    def get_firmwares(self) -> []:
+    @classmethod
+    def get_firmwares(cls) -> []:
         """
         :return: List of known firmwares
         """
--- tests/test_Server.py
+++ tests/test_Server.py
@@ -4,18 +4,15 @@ from server.router import Router
 
 
 class MyTestCase(unittest.TestCase):
-    s = None
-
     def setUp(self):
-        self.s = Server()
-        self.s.start(True, "../tests/config")
+        Server.start(True, "../tests/config")
         pass
 
     def tearDown(self):
-        self.s.stop()
+        Server.stop()
         pass
 
     def test_get_routers(self):
-        routers = self.s.get_routers()
+        routers = Server.get_routers()
         assert len(routers) != 0
         assert isinstance(routers[0], Router)
