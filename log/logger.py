5a1fea3d4558c0d6b9c048d52277aa3d3810deea
Sniger87 <tobi.ochs@googlemail.com>
2015-12-14 14:49:43 +0100

    refactor logger, add stream handler

--- log/logger.py
+++ log/logger.py
@@ -39,8 +39,10 @@ class Logger(metaclass=Singleton):
             return False
         return True
 
-    def setup(self, log_level: int = logging.INFO, file_path: str = "logger.log", file_log_level: int = logging.INFO,
-              log_formatter: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s") -> None:
+    def setup(self, log_level: int = logging.INFO, file_log_level: int = logging.INFO,
+              stream_log_level: int = logging.INFO, log_file_path: str = "logger.log",
+              log_file_formatter: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+              log_stream_formatter: str = "%(asctime)s - %(levelname)s - %(message)s") -> None:
 
         try:
             self._logger = logging.getLogger(__name__)
@@ -49,23 +51,31 @@ class Logger(metaclass=Singleton):
             self._logger.setLevel(log_level)
 
             # create a file handler
-            if file_path == "":
-                logging.error("Path is an empty string")
+            if log_file_path == "":
+                logging.error("Path of the log file is an empty string")
                 return
 
-            if file_path == "logger.log":
-                handler = logging.FileHandler(os.path.join(self.LOG_PATH, 'logger.log'))
+            if log_file_path == "logger.log":
+                file_handler = logging.FileHandler(os.path.join(self.LOG_PATH, 'logger.log'))
             else:
-                handler = logging.FileHandler(file_path)
+                file_handler = logging.FileHandler(log_file_path)
 
-            handler.setLevel(file_log_level)
+            file_handler.setLevel(file_log_level)
+
+            # create StreamHandler
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(stream_log_level)
 
             # create a logging format
-            formatter = logging.Formatter(log_formatter)
-            handler.setFormatter(formatter)
+            file_formatter = logging.Formatter(log_file_formatter)
+            file_handler.setFormatter(file_formatter)
+
+            stream_formatter = logging.Formatter(log_stream_formatter)
+            stream_handler.setFormatter(stream_formatter)
 
             # add the handlers to the logger
-            self._logger.addHandler(handler)
+            self._logger.addHandler(file_handler)
+            self._logger.addHandler(stream_handler)
 
         except logging.ERROR as ex:
             logging.error("Error at the setup of the logger object:\nError: {0}".format(ex))
@@ -79,6 +89,11 @@ class Logger(metaclass=Singleton):
         if not self.is_loaded:
             self.setup()
         self._logger.debug(msg, *args, **kwargs)
+
+    def warning(self, msg: str, *args, **kwargs) -> None:
+        if not self.is_loaded:
+            self.setup()
+        self._logger.warning(msg, *args, **kwargs)
 
     def error(self, msg: str, *args, **kwargs) -> None:
         if not self.is_loaded:
