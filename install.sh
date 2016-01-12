bc2781b0b37c740df246aab846b159feeb8db540
Kevin Rojczyk <kevin.roj@p8h.de>
2016-01-12 16:46:13 +0100

    correct relative path

--- install.sh
+++ install.sh
@@ -27,7 +27,8 @@ echo "[Install]
 WantedBy=multi-user.target" >> "$file"
 
 rm /bin/fftserver_alive
-ln test_server_alive.py /bin/fftserver_alive
+echo "#!/usr/bin/env bash
+python3.4 $DIR/test_server_alive.py" >> "/bin/fftserver_alive"
 chmod 755 /bin/fftserver_alive
 
 # activate service
