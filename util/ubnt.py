70f276ba799e103f83762b794792874e5f9c33da
lena <lena.held@web.de>
2016-01-12 17:07:32 +0100

    Ubiquiti mPower 6 control implemented, added "turn on power", "turn off power", "check if on or off"

--- util/ubnt.py
+++ util/ubnt.py
@@ -1,5 +1,6 @@
-import paramiko
 from util.power_strip import PowerStrip
+from log.logger import Logger
+from network.network_ctrl import NetworkCtrl
 
 
 class Ubnt(PowerStrip):
@@ -8,28 +9,80 @@ class Ubnt(PowerStrip):
         Different ports can be turned on or off, each port corresponds to a router
     """
 
+    def __init__(self, vlan_iface_name: str, vlan_iface_id: int, ip: str, ip_mask: int,
+                 usr_name: str, usr_password: str):
+
+        # Static IP of Ubiquiti mPower 6
+        self.ip = "192.168.1.20"
+        self.ip = ip
+
+        self.vlan_iface_id = None
+        self.vlan_iface_id = vlan_iface_id
+
+        self.vlan_iface_name = None
+        self.vlan_iface_name = vlan_iface_name
+
+        self.ip_mask = None
+        self.ip_mask = ip_mask
+
+        # Default Ubiquiti username
+        self.usr_name = "ubnt"
+        self.usr_name = usr_name
+
+        # Default Ubiquiti password
+        self.usr_password = "ubnt"
+        self.usr_password = usr_password
+
+        global network_ctrl
+        network_ctrl = NetworkCtrl(self)
+
     def connect(self):
-        ip = ""
-        usr = "ubnt"
-        pw = "ubnt"
+        """
+        Connects with the power strip via network controller
+        """
+        network_ctrl.connect_with_remote_system()
 
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, 22, usr, pw)
-
-        return ssh
+    def __command(self, cmd):
+        output = network_ctrl.send_command(cmd)
+        Logger().info("Command \'" + str(cmd) + "\' sent to power strip")
+        return output
 
     def port_status(self, port_id):
-        pass
+        """
+        Checks if power on port is on or off
+        :param port_id: ID of power strip port
+        :return: 1 for on or 0 for off
+        """
+        cmd = "cat /dev/output" + str(port_id)
+        output = self.__command(cmd)
+        return output
 
     def up(self, port_id):
-        command = "echo 1 > /dev/output" + str(port_id)
-        ssh = self.connect()
-        stdin, stdout, stderr = ssh.exec_command(command)
-        ssh.close()
+        """
+        Turns on power on port
+        :param port_id: ID of power strip port
+        :return: bool for failure or success
+        """
+        cmd = "echo 1 > /dev/output" + str(port_id)
+        self.__command(cmd)
+        if self.port_status(port_id) == 1:
+            Logger().info("Power turned on: port " + str(port_id))
+            return True
+        else:
+            Logger().error("Could not turn on power on port " + str(port_id))
+            return False
 
     def down(self, port_id):
-        command = "echo 0 > /dev/output" + str(port_id)
-        ssh = self.connect()
-        stdin, stdout, stderr = ssh.exec_command(command)
-        ssh.close()
+        """
+        Turns off power on port
+        :param port_id: ID of power strip port
+        :return: bool for failure or success
+        """
+        cmd = "echo 0 > /dev/output" + str(port_id)
+        self.__command(cmd)
+        if self.port_status(port_id) == 0:
+            Logger().info("Power turned off: port " + str(port_id))
+            return True
+        else:
+            Logger().error("Could not turn off power on port " + str(port_id))
+            return False
