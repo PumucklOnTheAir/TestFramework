b1b6d0e4b35ae6a8c5957b4b8ddd65569829c9bc
Kevin Rojczyk <kevin.roj@p8h.de>
2016-01-18 11:59:00 +0100

    update new unittest config path

--- framework_unittests/test_Server_2.py
+++ framework_unittests/test_Server_2.py
@@ -29,7 +29,7 @@ class ServerTestCase2(unittest.TestCase):
     @staticmethod
     def serverStartWithParams():
         base_dir = os.path.dirname(os.path.dirname(__file__))  # This is your Project Root
-        config_path = os.path.join(base_dir, 'tests/configs/config_no_vlan')  # Join Project Root with config
+        config_path = os.path.join(base_dir, 'framework_unittests/configs/config_no_vlan')
         Server.start(config_path=config_path)
 
     def setUp(self):
