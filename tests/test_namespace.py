355c98ac8ab4ca4a473febd156943fe204758296
Kevin Rojczyk <kevin.roj@p8h.de>
2016-02-01 10:05:04 +0100

    demo for entering an network namespace

--- tests/test_namespace.py
+++ tests/test_namespace.py
@@ -3,6 +3,9 @@ from network.vlan import Vlan
 from network.namespace import Namespace
 from subprocess import Popen, PIPE
 from log.logger import Logger
+from multiprocessing import Process
+import os
+import time
 
 
 class TestNamespace(TestCase):
@@ -33,3 +36,47 @@ class TestNamespace(TestCase):
             process = Popen(["ip", "netns"], stdout=PIPE, stderr=PIPE)
             stdout, sterr = process.communicate()
             assert stdout.decode('utf-8') == ""
+
+    # demonstrates only the workflow auf enetering an existing namespace
+    def test_enterns(self):
+        print("Beginn test_enterns")
+
+        p = Process(target=self.process1, args=())
+        p.start()
+        # p.join() # something in Vlan or Namespace hanging this process up
+        time.sleep(2)
+
+        p2 = Process(target=self.process2, args=())
+        p2.start()
+        p2.join()
+
+    def process1(self):
+        print("Process 1")
+        # Create VLAN
+        vlan = Vlan('eth0', 'vlan21', 10, '192.168.1.10', 24)
+        assert isinstance(vlan, Vlan)
+        vlan.create_interface("192.168.1.11", 24)
+
+        # Create Namespace
+        namespace = Namespace('nsp21', vlan.ipdb)
+        assert isinstance(namespace, Namespace)
+
+        # encapsulate VLAN
+        namespace.encapsulate_interface(vlan.vlan_iface_name)
+
+        os.system('ip link list')
+
+        # time.sleep(6)
+
+    def process2(self):
+        print('Process2')
+        print("before")
+        os.system('ip link list')
+
+        import ctypes
+        libc = ctypes.CDLL('libc.so.6')
+        fd = open('/var/run/netns/nsp21')
+        libc.setns(fd.fileno(), 0)
+
+        print("after")
+        os.system('ip link list')
