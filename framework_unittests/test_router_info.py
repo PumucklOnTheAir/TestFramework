bf13072baed652692d48e432c439f822764c6341
symen <symenschmitt@web.de>
2016-02-04 12:03:39 +0100

    add NVAssistent

--- framework_unittests/test_router_info.py
+++ framework_unittests/test_router_info.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from router.router import Router, Mode
 from util.router_info import RouterInfo
+from network.nv_assist import NVAssistent
+from pyroute2 import netns
 
 
 class TestRouterInfo(TestCase):
@@ -8,16 +10,25 @@ class TestRouterInfo(TestCase):
     def test_router_info(self):
         router = self._create_router()
 
+        # NVAssisten
+        nv_assist = NVAssistent("eth0")
+        nv_assist.create_namespace_vlan(router)
+        # Set netns for the current process
+        netns.setns(router.namespace_name)
+
         router_info = RouterInfo(router)
         router_info.start()
         router_info.join()
 
+        # Close Namespaces and VLANs
+        nv_assist.close()
+
     def _create_router(self):
         # Create router
-        router = Router(0, "vlan21", 21, "10.223.254.254", 16, "192.168.1.1", 24, "root", "root", 1)
+        router = Router(0, "vlan21", 21, "10.223.254.254", 16, "192.168.2.13", 24, "bananapi", "bananapi", 1)
         router.model = "TP-LINK TL-WR841N/ND v9"
         router.mac = "e8:de:27:b7:7c:e2"
         # Has to be matched with the current mode (normal, configuration)
-        router.mode = Mode.normal
+        router.mode = Mode.configuration
         assert isinstance(router, Router)
         return router
