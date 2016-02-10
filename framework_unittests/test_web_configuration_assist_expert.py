1310164a49a4f7d3fdab8601f80be91eef6db41c
Symen <symenschmitt@web.de>
2016-02-10 15:42:49 +0100

    bugfixing; nv_assistent wasn't used

--- framework_unittests/test_web_configuration_assist_expert.py
+++ framework_unittests/test_web_configuration_assist_expert.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
-
-from network.network_ctrl import NetworkCtrl
+from pyroute2 import netns
+from network.nv_assist import NVAssistent
 from router.router import Router, Mode
 from config.configmanager import ConfigManager
 from util.router_setup_web_configuration import RouterWebConfiguration
+
 
 class TestWebConfigurationAssistExpert(TestCase):
 
@@ -13,18 +14,22 @@ class TestWebConfigurationAssistExpert(TestCase):
         It sets the values of all the  from WebInterface of the Router.
         """
         router = self._create_router()
+        # NVAssisten
+        nv_assist = NVAssistent("eth0")
+        nv_assist.create_namespace_vlan(router)
+        # Set netns for the current process
+        netns.setns(router.namespace_name)
+
         # Config
-        config = ConfigManager().get_web_interface_dict()[0]
+        config = ConfigManager().get_web_interface_dict()[router.id]
         self.assertEqual(len(config), 30, "Wrong size of the Config-Directory")
-        # Create NetworkCrtl
-        network_ctrl = NetworkCtrl(router)
-        assert isinstance(network_ctrl, NetworkCtrl)
 
         router_web_config = RouterWebConfiguration(router, config, wizard=False)
         router_web_config.start()
         router_web_config.join()
 
         assert router.mode == Mode.configuration
+        nv_assist.close()
 
     def _create_router(self):
         # Create router
@@ -32,6 +37,6 @@ class TestWebConfigurationAssistExpert(TestCase):
         router.model = "TP-LINK TL-WR841N/ND v9"
         router.mac = "e8:de:27:b7:7c:e2"
         # Has to be matched with the current mode (normal, configuration)
-        router.mode = Mode.normal
+        router.mode = Mode.configuration
         assert isinstance(router, Router)
         return router
