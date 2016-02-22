f644a74ea86ca41f1cfe8a98c4e6daf11437eff2
Kevin Rojczyk <kevin.roj@p8h.de>
2016-02-22 16:10:59 +0100

    repair test_server_alive.py
    and added it to travis.yml

--- test_server_alive.py
+++ test_server_alive.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import unittest
-import time
 from server.server import Server
 from multiprocessing import Process
 from server.ipc import IPC
-import os
+from framework_unittests.test_Server_2 import block_until_server_is_online
+import time
 
 
 server_already_started = False
@@ -13,11 +13,10 @@ server_already_started = False
 
 class ServerAlive(unittest.TestCase):
     """
-    Little test which tests if he can connect as a client to the server.
+    Little test which tests if it is possible to connect as a client to the test server.
     If you are running this module standalone it will try to connect to an all ready started server.
     If not it creates his own server instance.
     """""
-    # TODO This test seems to be broken
     proc = None
 
     @classmethod
@@ -25,7 +24,7 @@ class ServerAlive(unittest.TestCase):
         if not server_already_started:
             #  starts the IPC server in another(!) process
             cls.proc = Process(target=ServerAlive.serverStartWithParams, args=()).start()
-            time.sleep(2)
+            block_until_server_is_online()
 
     @classmethod
     def tearDownClass(cls):
@@ -37,18 +36,25 @@ class ServerAlive(unittest.TestCase):
 
     @staticmethod
     def serverStartWithParams():
-        base_dir = os.path.dirname(os.path.dirname(__file__))  # This is your Project Root
-        config_path = os.path.join(base_dir, 'tests/configs/config_no_vlan')  # Join Project Root with config
-        Server.start(config_path=config_path)
+        Server.start(config_path='framework_unittests/configs/config_no_vlan')
 
     def setUp(self):
         self.ipc_client = IPC()
         self.ipc_client.connect()
         self.server_proxy = self.ipc_client.get_server_proxy()
 
+        if not server_already_started:
+            # wait until server has all tasks before executed
+            for i in range(2):  # do it two times to be sure
+                routers = self.server_proxy.get_routers()
+                for router in routers:
+                    while not not self.server_proxy.get_routers_task_queue_size(router.id):
+                        time.sleep(2)
+                        print('.', end="", flush=True)
+
     def test_get_version(self):
         version = self.server_proxy.get_server_version()
-        assert len(version) != 0
+        assert version == Server.VERSION
 
 if __name__ == '__main__':
     server_already_started = True
