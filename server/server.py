8bd223f64ddd24953ac6e3c1231288bd2f09e413
Kevin Rojczyk <kevin.roj@p8h.de>
2016-02-23 16:13:00 +0100

    update all in router_online

--- server/server.py
+++ server/server.py
@@ -535,7 +535,7 @@ class Server(ServerProxy):
         return None
 
     @classmethod
-    def router_online(cls, router_ids: List[int], all: bool) -> None:
+    def router_online(cls, router_ids: List[int], update_all: bool) -> None:
         """
         Tries to connect to the `Router` and updates the Mode of the Router.
 
@@ -543,7 +543,7 @@ class Server(ServerProxy):
         :param update_all: Is True if all Routers should be updated
         """
         from util.router_online import RouterOnlineJob  # TODO remove it from here #64
-        if all:
+        if update_all:
             for router in cls.get_routers():
                 cls.start_job(router, RouterOnlineJob())
         else:
