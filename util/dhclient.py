dc26f0212404c5d2a68b77ec148b2e6888fadd2f
symen <symenschmitt@web.de>
2016-02-23 15:07:26 +0100

    bugfixing

--- util/dhclient.py
+++ util/dhclient.py
@@ -1,4 +1,6 @@
 from subprocess import Popen, PIPE
+import socket, struct, fcntl
+import time
 
 
 class Dhclient:
@@ -17,6 +19,8 @@ class Dhclient:
         try:
             process = Popen(['dhclient', interface], stdout=PIPE, stderr=PIPE)
             stdout, stderr = process.communicate()
+            while Dhclient.get_ip(interface) is None:
+                time.sleep(0.5)
             if "File exists" in str(stderr):
                 return 2
             elif stderr.decode('utf-8') != "":
@@ -26,3 +30,19 @@ class Dhclient:
             return 0
         except Exception as e:
             raise e
+
+    @staticmethod
+    def get_ip(interface) -> str:
+        """
+        Gets the ip of a specific interface
+        :return: the ip of an interface without network-mask
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockfd = sock.fileno()
+        ifreq = struct.pack('16sH14s', interface.encode('utf-8'), socket.AF_INET, b'\x00' * 14)
+        try:
+            res = fcntl.ioctl(sockfd, 0x8915, ifreq)
+        except:
+            return None
+        ip = struct.unpack('16sH2x4s8x', res)[2]
+        return socket.inet_ntoa(ip)
