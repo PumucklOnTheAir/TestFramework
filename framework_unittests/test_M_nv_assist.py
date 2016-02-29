3431cd3fa356828b15a7ce9b8f9bc0facf27cd81
symen <symenschmitt@web.de>
2016-02-29 15:15:27 +0100

    test-functions extended

--- framework_unittests/test_M_nv_assist.py
+++ framework_unittests/test_M_nv_assist.py
@@ -1,18 +1,43 @@
 from unittest import TestCase
 from network.nv_assist import NVAssistent
 from router.router import Router, Mode
+from subprocess import Popen, PIPE
 
 
 class TestNVAssist(TestCase):
+    """
+    This TestModule tests the functionality of the NV_Assistent.
+
+        1. Create Router (Mode: normal)
+        2. Try to create a nv_assistent
+            - create vlan
+            - create namespace
+            - encapsulate vlan in namespace
+    """""
 
     def test_create_namespace_vlan_veth(self):
         router = self._create_router()
 
+        print("Test NVAssistent ...")
         for i in range(0, 2):
-            print("Test" + str(i) + ":")
+            print("Test" + str(i))
             nv_assi = NVAssistent("eth0")
             assert isinstance(nv_assi, NVAssistent)
             nv_assi.create_namespace_vlan(router)
+
+            # Test if the Namespace now exists
+            process = Popen(["ip", "netns"], stdout=PIPE, stderr=PIPE)
+            stdout, sterr = process.communicate()
+            assert sterr.decode('utf-8') == ""
+            assert router.namespace_name in stdout.decode('utf-8')
+
+            # Test if the VLAN now in Namespace exists
+            process = Popen(["ip", "netns", "exec", router.namespace_name,
+                             "ip", "link", "show", "dev", router.vlan_iface_name], stdout=PIPE, stderr=PIPE)
+            stdout, sterr = process.communicate()
+            assert sterr.decode('utf-8') == ""
+            assert router.vlan_iface_name in stdout.decode('utf-8')
+
             nv_assi.close()
 
     def _create_router(self):
