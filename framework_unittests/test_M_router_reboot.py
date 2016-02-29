5d27e12e6ed7f0106ce3928c77e3694db3b72069
symen <symenschmitt@web.de>
2016-02-29 16:40:12 +0100

    bugfixing

--- framework_unittests/test_M_router_reboot.py
+++ framework_unittests/test_M_router_reboot.py
@@ -3,9 +3,8 @@ from pyroute2 import netns
 from router.router import Router, Mode
 from util.router_reboot import RouterReboot
 from network.nv_assist import NVAssistent
-import logging
-import time
 from multiprocessing import Process, Queue
+from util.router_online import RouterOnline
 
 
 class TestRouterReboot(TestCase):
@@ -19,17 +18,18 @@ class TestRouterReboot(TestCase):
     """""
 
     def test_reboot(self):
+        print("Test the 'reboot'-function")
         router = self._create_router()
         q = Queue()
         # Reboot Router, in an own thread, into configmode
         p = Process(target=self._reboot_into_config, args=(router, q,))
         p.start()
+        print("Wait till the Router has rebooted ...")
         # Get result from process
         router = q.get()
         p.join()
 
-        logging.debug("Wait 1min till the Router has rebooted.")
-        time.sleep(60)
+        print("Router Mode: " + str(router.mode))
 
         # Reboot Router, in an own thread, into normalmode
         p = Process(target=self._reboot_into_normal, args=(router, q,))
@@ -39,7 +39,7 @@ class TestRouterReboot(TestCase):
         p.join()
 
     def _reboot_into_config(self, router: Router, q: Queue):
-        logging.debug("Reboot Router into configmode...")
+        print("Reboot Router into configmode ...")
         # Create NVAssistent
         nv_assist = NVAssistent("eth0")
         nv_assist.create_namespace_vlan(router)
@@ -55,7 +55,7 @@ class TestRouterReboot(TestCase):
         q.put(router)
 
     def _reboot_into_normal(self, router: Router, q: Queue):
-        logging.debug("Reboot Router back into normalmode...")
+        print("Reboot Router back into normalmode ...")
         nv_assist = NVAssistent("eth0")
         nv_assist.create_namespace_vlan(router)
         # Set netns for the current process
@@ -68,7 +68,7 @@ class TestRouterReboot(TestCase):
         nv_assist.close()
         q.put(router)
 
-    def _create_router(self):
+    def _create_router(self) -> Router:
         # Create router
         router = Router(0, "vlan21", 21, "10.223.254.254", 16, "192.168.1.1", 24, "root", "root", 1)
         router.model = "TP-LINK TL-WR841N/ND v9"
