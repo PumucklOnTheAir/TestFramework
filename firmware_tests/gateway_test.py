a4140eccbcea19c251be25d784fcd0cdb253cca8
symen <symenschmitt@web.de>
2016-03-17 15:40:25 +0100

    bugfixing

--- firmware_tests/gateway_test.py
+++ firmware_tests/gateway_test.py
@@ -1,6 +1,5 @@
 from server.test import FirmwareTest
 from network.network_ctrl import NetworkCtrl
-from typing import List
 
 
 class GatewayTest(FirmwareTest):
@@ -11,14 +10,15 @@ class GatewayTest(FirmwareTest):
 
     def test_connection(self):
         network_ctrl = NetworkCtrl(self.remote_system)
+        network_ctrl.connect_with_remote_system()
         ping_result1 = network_ctrl.send_command("ping -c 5 8.8.8.8 | grep received")
         ping_result2 = network_ctrl.send_command("ping -c 5 freifunk.net | grep received")
-        assert (self._ping_successful(ping_result1) or self._ping_successful(ping_result2))
+        assert (self._ping_successful(ping_result1[0]) or self._ping_successful(ping_result2[0]))
+        network_ctrl.exit()
 
-    def _ping_successful(self, ping_result_lst: List)->bool:
-        i = 5
-        for ping_result in ping_result_lst:
-            if str(i) + " received" in ping_result:
+    def _ping_successful(self, ping_result: str)->bool:
+        for i in range(0, 5):
+            tmp = str(5-i) + " packets received"
+            if tmp in ping_result:
                 return True
-            i -= 1
         return False
