012dbc4a13470569254d84fdd2b3f9d2003312c9
lena <lena.held@web.de>
2016-03-22 19:22:46 +0100

    test in cli util fixed

--- framework_unittests/test_AP_cli_util.py
+++ framework_unittests/test_AP_cli_util.py
@@ -7,8 +7,10 @@ class MyTestCase(unittest.TestCase):
         global cli_util
         cli_util = CLIUtil()
         assert isinstance(cli_util, CLIUtil)
+        self._test_output_detailed()
+        self._test_status_all()
 
-    def test_output_detailed(self):
+    def _test_output_detailed(self):
         info = [["ID", "0"],
                 ["Model", "Modell X"],
                 ["MAC", "00:00:00:00:00:00"],
@@ -27,23 +29,23 @@ class MyTestCase(unittest.TestCase):
                     "111.111.1.1, " + "222.222.222.222"],
                    ["1", "Name2", "00:00:00:00:00:00", "STATUS", "0.0.0.0, " +
                     "111.111.1.1, " + "222.222.222.222"]]
-        if_list_headers = ["ID", "Name", "MAC", "Status", "IP Addresses"]
+        if_list_headers = ["ID", "Name", "MAC", "Status", "Wifi Info"]
 
         mem_list = [["Used", "3/5"],
                     ["Free", "2/5"],
                     ["Shared", "5"],
                     ["Buffers", "5"]]
         proc_list_headers = ["Proc"]
-        proc_list = ["Process1"]
+        proc_list = [["Process1"]]
         socket_list_headers = ["Socket"]
-        socket_list = ["Socket1"]
+        socket_list = [["Socket1"]]
         bat_list_headers = ["Bat Originator"]
-        bat_list = ["List Bat Originator"]
+        bat_list = [["List Bat Originator"]]
 
         cli_util.print_router(info, if_list_headers, if_list, proc_list_headers, proc_list,
                               socket_list_headers, socket_list, mem_list, bat_list_headers, bat_list)
 
-    def test_status_all(self):
+    def _test_status_all(self):
         headers = ["ID", "Router Model/Vers", "VLAN ID", "Router Name", "IP", "MAC"]
         content = [["0", "Model X", "21", "Freifunk", "0.0.0.0", "00:00:00:00:00:00"]]
         cli_util.print_dynamic_table(content, headers)
