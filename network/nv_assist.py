a0eaa1e7307b7f2cd4fa8c20566f90e653f3be85
Symen <symenschmitt@web.de>
2016-03-23 10:35:09 +0100

    cleanup docs and comments; remove veth-attribute

--- network/nv_assist.py
+++ network/nv_assist.py
@@ -8,11 +8,14 @@ import logging
 
 class NVAssistent:
     """
-    1. Creates a IPDB: stores interfaces
-    2. Creates a Bridge: connects multiple interfaces via a virtual switch
-    3. Creates VLANs
-    4. Creates Veths: connects a Namespace with the Bridge
-    5. Creates Namespaces: encapsulte a given Interface
+    The NVAssistent (NamespaceVlanAssistent) provides the following Features:
+        1. Creates a IPDB: stores the Network-Interfaces
+        2. Creates VLANs and Namespaces
+        3. Encapsulates the VLANs inside the Namespaces
+        4. Delete VLANs and Namespaces
+
+    IPDB:   Is a transactional database, containing records, representing network stack objects.
+            Any change in the database is not reflected immidiately in OS, but waits until commit() is called.
 
     If we want to use VLANs for Systems (with identical IPs) on the network, we need Namespaces. Each Namespace
     encapsulate one VLAN for the current process. The Namespace only contains the given VLAN and the 'lo'-interface.
@@ -22,21 +25,18 @@ class NVAssistent:
 
     def __init__(self, link_iface_name: str):
         """
-        Initialize the IPDB for the interfaces and creates a Bridge (virtual switch).
-
-        :param link_iface_name: 'physical'-interface like 'eth0'
+        :param link_iface_name: 'physical'-interface like 'eth0' to which the VLAN is connected to
         """
         self.ipdb = IPDB()
         self.link_iface_name = link_iface_name
         self.vlan_dict = dict()
-        self.veth_dict = dict()
         self.nsp_dict = dict()
 
     def create_namespace_vlan(self, remote_system: RemoteSystem):
         """
         Creats a Namespace and a VLAN. Encapsulate the VLAN inside the Namespace.
 
-        :param remote_system: Router or powerstrip
+        :param remote_system: Router-Obj or Powerstrip-Obj with which we want to connect to.
         """
         if remote_system.namespace_name in self.nsp_dict.keys():
             logging.debug("%s[-] Namespace already exists", LoggerSetup.get_log_deep(2))
@@ -54,9 +54,9 @@ class NVAssistent:
         """
         Returns the the first IP of the VLAN in the IPDB of the Namespace
 
-        :param namespace_name: Namespace name
-        :param vlan_iface_name: VLAN name
-        :return: IP address (ip, mask)
+        :param namespace_name: Namespace_name
+        :param vlan_iface_name: VLAN_name
+        :return: IP address (ip/mask)
         """
         ip_address = self.nsp_dict[namespace_name].ipdb_get_ip(ipdb=False, iface_name=vlan_iface_name).split("/")
         ip = ip_address[0]
@@ -64,22 +64,27 @@ class NVAssistent:
         return ip, mask
 
     def delete_vlan(self, vlan_iface_name: str):
+        """
+        Removes the given VLAN.
+
+        :param vlan_iface_name: VLAN_name
+        """
         self.vlan_dict[vlan_iface_name].delete_interface()
 
-    def delete_veth(self, veth_iface_name: str):
-        self.veth_dict[veth_iface_name].delete_interface()
+    def delete_namespace(self, namespace_name: str):
+        """
+        Removes the given Namespace.
 
-    def delete_namespace(self, nsp_name: str):
-        self.nsp_dict[nsp_name].remove()
+        :param namespace_name: Namespace_name
+        """
+        self.nsp_dict[namespace_name].remove()
 
     def close(self):
         """
-        Deletes all VLANs, 'veth'-interfaces and Namespaces
+        Deletes all VLANs and Namespaces and releases the IPDB.
         """
         for vlan in self.vlan_dict:
             self.delete_vlan(vlan)
-        for veth in self.veth_dict:
-            self.delete_veth(veth)
         for nsp in self.nsp_dict:
             self.delete_namespace(nsp)
         self.ipdb.release()
