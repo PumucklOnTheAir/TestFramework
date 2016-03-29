8ef408fd9599f2e923c7e1d23a2ba52ac1629dd5
symen <symenschmitt@web.de>
2016-03-29 12:12:40 +0200

    bugfixing

--- firmware_tests/test_C_accessibility_web_config.py
+++ firmware_tests/test_C_accessibility_web_config.py
@@ -15,7 +15,7 @@ class TestAccessibilityWebConfig(FirmwareTest):
         """
         Test:
             1. Router is in configuration-mode
-            2. The Title of the wizard-page isn't Null => wizard-page exist
+            2. The page-source of the wizard-page doesn't contain "Not Found" => wizard-page exist
         """
         logging.debug("%sTest: Accessibility of the WebInterface: Wizard-Page", LoggerSetup.get_log_deep(1))
         assert self.remote_system.mode == Mode.configuration
@@ -23,8 +23,8 @@ class TestAccessibilityWebConfig(FirmwareTest):
         pre_command = ['ip', 'netns', 'exec', self.remote_system.namespace_name]
         browser = WebdriverPhantomjsExtended(pre_command=pre_command)
         browser.get(self.remote_system.ip)
-        browser.get('http://' + self.remote_system.ip + '/cgi-bin/luci/gluon-config-mode/')
-        self.assertEqual(browser.title != "", "The Wizard-Page isn't accessible")
+        browser.get('http://' + self.remote_system.ip)
+        self.assertTrue("Not Found" not in browser.page_source, "The Wizard-Page isn't accessible")
         logging.debug("%s[" + u"\u2714" + "] The Wizard-Page is accessible", LoggerSetup.get_log_deep(2))
         browser.close()
 
@@ -32,14 +32,15 @@ class TestAccessibilityWebConfig(FirmwareTest):
         """
         Test:
             1. Router is in configuration-mode
-            2. The Title of the expert-page isn't Null => expert-page exist
+            2. The page-source of the expert-page doesn't contain "Not Found" => expert-page exist
         """
         logging.debug("%sTest: Accessibility of the WebInterface: Expert-Page", LoggerSetup.get_log_deep(1))
         assert self.remote_system.mode == Mode.configuration
+        logging.debug("%s[" + u"\u2714" + "] Correct Mode", LoggerSetup.get_log_deep(2))
         pre_command = ['ip', 'netns', 'exec', self.remote_system.namespace_name]
         browser = WebdriverPhantomjsExtended(pre_command=pre_command)
         browser.get(self.remote_system.ip)
         browser.get('http://' + self.remote_system.ip + '/cgi-bin/luci/admin/')
-        self.assertTrue(browser.title != "", "The Expert-Page isn't accessible")
+        self.assertTrue("Not Found" not in browser.page_source, "The Expert-Page isn't accessible")
         logging.debug("%s[" + u"\u2714" + "] The Expert-Page is accessible", LoggerSetup.get_log_deep(2))
         browser.close()
